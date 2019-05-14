95d4219d00ab81f9d24701cc59aa931a9292cbc8
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-05-14 10:22:14 -0400

    se suben archivos de html básico,funcion filtrar y mostrar data

--- src/data.js
+++ src/data.js
@@ -1,10 +1,34 @@
 /* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+const filterByTag = (data, tag) => {
+  if (tag === 'all') {
+    return data;
+  }
 
-const example = () => {
-  return 'example';
+  return data.filter(element => {
+    return element.tags.includes(tag);
+  });
+}
+
+window.filterByTag = filterByTag;
+
+
+// ordenar
+const orderLol = (data, sortBy, sortOrder) => {
+
+  const list = data.sort((a, b) => {
+    if (a[sortBy] > b[sortBy])
+      return 1;
+    if (a[sortBy] < b[sortBy])
+      return -1;
+    return 0;
+  });
+
+  if (sortOrder === 'za') {
+    return list.reverse();
+  }
+
+  return list;
 };
 
-window.example = example;
+window.orderLol = orderLol;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,57 @@
-/* Manejo del DOM */
+//Referencias a los elementos del DOM
+ const lolList = document.getElementById('lol_list')
+ const selectTag= document.getElementById('select_tag');
+ const sortLol= document.getElementById('select_order');
+ const btnFilter = document.getElementById('filter');
+
+
+//Se deja referencia a la data 
+let data = Object.values(window.LOL.data);
+
+//Funcion mostrar data dentro de una tarjeta de bootstrap
+const showData = (data) => {
+    let html = '';
+  
+    data.forEach(element => {
+      html += `
+      <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 py-3">
+        <div class="card h-100">
+          <div class="card-img-wrapper">
+          <img class="card-img mx-auto d-block" src="${element.img}" alt="${element.name}">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">HP: ${element.stats.spellblock}</h5>
+            <p class="card-text">Id: ${element.id}</p>
+            <p class="card-text">Ataque: ${element.info.attack}</p>
+            <p class="card-text">Defensa: ${element.info.defense}</p>
+            <p class="card-text">Magic: ${element.info.magic}</p>
+            <p class="card-text">Dificultad: ${element.info.difficulty}</p>
+
+          </div>
+        </div>
+      </div>`;
+    });
+  //Imprimir tarjeta en html
+  
+  lolList.innerHTML = html
+}
+
+
+
+//Cuando termine de cargar la página va a desplegar la lista
+window.addEventListener('load', () => {
+  showData(window.filterByTag(data, 'all'));
+})
+
+
+//Filtro múltiple tags y ordenar
+btnFilter.addEventListener('click', () => {
+  const tag = selectTag.value;
+  const order = sortLol.value;
+
+  let list = window.filterByTag(data, tag);
+  list = window.orderLol(list,"rol", order);
+
+  showData(list);
+
+})
