46a2b59e35664d4636809b8f66d6390e3f11875f
JoceCasteletti <jocelyncasteletti@gmail.com>
2019-05-16 09:36:12 -0400

    se sube fetch y mejoras de css en tarjetas

--- src/main.js
+++ src/main.js
@@ -1,63 +1,58 @@
-//Referencias a los elementos del DOM
- const lolList = document.getElementById('lol_list')
- const selectTag= document.getElementById('select_tag');
- const sortLol= document.getElementById('select_order');
- const btnFilter = document.getElementById('filter');
-
-
-//Se deja referencia a la data 
-let data = Object.values(window.LOL.data);
-
-window.addEventListener(‘load’, () => {
-  fetch(‘data/LOL/data.json’).then((response) => {
-      return response.json();
-  }).then((data) => {
-      const list = data.LOL;
-
-//Funcion mostrar data dentro de una tarjeta de bootstrap
-const showData = (data) => {
-    let html = '';
-  
-    data.forEach(element => {
-      html += `
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 py-3">
-        <div class="card h-100">
-          <div class="card-img-wrapper">
-          <img class="card-img mx-auto d-block" src="${element.img}" alt="${element.name}">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Spellblock: ${element.stats.spellblock}</h5>
-            <p class="card-text">Id: ${element.id}</p>
-            <p class="card-text">Ataque: ${element.info.attack}</p>
-            <p class="card-text">Defensa: ${element.info.defense}</p>
-            <p class="card-text">Magic: ${element.info.magic}</p>
-            <p class="card-text">Dificultad: ${element.info.difficulty}</p>
-
-          </div>
-        </div>
-      </div>`;
-    });
-  //Imprimir tarjeta en html
-  
-  lolList.innerHTML = html
-}
-
-
-
-//Cuando termine de cargar la página va a desplegar la lista
 window.addEventListener('load', () => {
-  showData(window.filterByTag(data, 'all'));
-})
+  //Referencias a los elementos del DOM
+  const lolList = document.getElementById('lol_list')
+  const selectTag = document.getElementById('select_tag');
+  const sortLol = document.getElementById('select_order');
 
+  //Funcion mostrar data dentro de una tarjeta de bootstrap
+  const showData = (data) => {
+    let html = '';
 
-//Filtro múltiple tags y ordenar
-btnFilter.addEventListener('click', () => {
-  const tag = selectTag.value;
-  const order = sortLol.value;
+    data.forEach(element => {
+      if (!element.img) {
+        element.img = 'https://via.placeholder.com/120';
+      }
 
-  let lolList = window.filterByTag(data, tag);
-  lolList = window.orderLol(lolList,"rol", order);
+      html += `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 py-3">
+      <div class="card h-100">
+        <div class="card-img-wrapper">
+        <img class="card-img mx-auto d-block" src="${element.img}">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Nombre: ${element.id}</h5>
+          <p class="card-text">Ataque: ${element.info.attack}</p>
+          <p class="card-text">Defensa: ${element.info.defense}</p>
+          <p class="card-text">Magia: ${element.info.magic}</p>
+          <p class="card-text">Dificultad: ${element.info.difficulty}</p>
+        </div>
+      </div>
+    </div>`;
+    });
+    //Imprimir tarjeta en html
 
-  showData(lolList);
+    lolList.innerHTML = html
+  };
 
+  fetch('https://raw.githubusercontent.com/JoceCasteletti/LOL-Joce/master/src/data/lol/lol.json')
+  .then(response => response.json())
+  .then(obj => {
+    const data = Object.values(obj.data);
+
+    const filter = () => {
+      const tag = selectTag.value;
+      const order = sortLol.value;
+  
+      let lolList = window.filterByTag(data, tag);
+      lolList = window.orderLol(lolList, "rol", order);
+  
+      showData(lolList);
+    }
+    
+    //Filtro múltiple tags y ordenar
+    selectTag.addEventListener('change', filter);
+    sortLol.addEventListener('change', filter);
+
+    showData(window.filterByTag(data, 'all'));
+  });
 })
--- test/data.spec.js
+++ test/data.spec.js
@@ -2,15 +2,69 @@ global.window = global;
 global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
+const data = [
+
+  {
+    name: "Aatrox",
+    tags:"Fighter", 
+    info: { difficulty: 4 }
+  },
+  {
+    name: "Ashe",
+    tags: "Marksman",
+    info: { difficulty: 4 }
+  },
+  {
+    name: "Zyra",
+    tags: "Mage",
+    info: { difficulty: 7 }
+  },
+  {
+    name: "Veigar",
+    tags: "Mage",
+    info: { difficulty: 7 }
+  }
+
+]
 
 
-describe('example', () => {
+describe('filterByTag', () => {
   
   it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+    assert.equal(typeof window.filterByTag, 'function');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  it('debería retornar Veigar para filtrar por nombre', () => {
+    assert.deepEqual(window.filterByTag(data,"Veigar"),
+    {
+      name: "Veigar",
+      tags: "Mage",
+      info: { difficulty: 7 }
+    }
+    );
   });
 })
+
+
+describe('orderLol', () => {
+
+  it('debería ser una función', () => {
+    assert.equal(typeof window.orderLol, 'function');
+  });
+  
+  it('debería retornar los champion ordenado de la A a la Z', () => {
+    assert.deepEqual(window.orderLol(data, "name", "az"), 
+  )});
+
+  it('debería retornar ["Aatrox","Ashe","Veigar","Zyra"] para champsOrder "00",orden de "A-Z"', () => {
+    assert.deepEqual(window.orderLol(data,"00"), [
+    {name: "Aatrox", tags:"Fighter",info: { difficulty: 4 }},
+    {name: "Ashe", tags:"Marksman", info: { difficulty: 4 }},
+    {name:"Veigar",tags:"Mage",info: { difficulty: 7 }},
+    {name:"Zyra", tags:"Mage",info: { difficulty: 7 }}
+  ]);
+    
+})}
+
+
+  
